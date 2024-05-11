a60f4374bd96b7c0ec7ec52602b4203742efbf64
Spiros Makris <smakris@iit.demokritos.gr>
2024-05-11 15:30:15 +0300

    fix missing try

--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,10 @@ pub fn main() !void {
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // try stdout.print("Logs from your program will appear here!", .{});
-    try stdout.print("Ziggy Redis", .{});
+    try stdout.print("Ziggy Redis\n", .{});
 
     const address = try net.Address.resolveIp("127.0.0.1", 6379);
-    var listener = address.listen(.{
+    var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
@@ -24,15 +24,15 @@ pub fn main() !void {
     // Uncomment this block to pass the first stage
     //
     // const address = try net.Address.resolveIp("127.0.0.1", 6379);
-    //
+
     // var listener = try address.listen(.{
     //     .reuse_address = true,
     // });
     // defer listener.deinit();
-    //
+
     // while (true) {
     //     const connection = try listener.accept();
-    //
+
     //     try stdout.print("accepted new connection", .{});
     //     connection.stream.close();
     // }
